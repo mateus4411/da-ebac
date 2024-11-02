5b75026e8f63107738f6b1225eb6a9de1f4a1781
mateus4411 <mateuscorreia1999@gmail.com>
2024-11-02 06:59:45 +0000

    arquivo gasolina.py alterado em dev

--- gasolina.py
+++ gasolina.py
@@ -3,5 +3,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('gasolina.csv')
-sns.lineplot(data=df, x='dia', y='venda', marker='o')
-plt.savefig('gasolina.png')
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(10, 6))
+
+grafico = sns.lineplot(data=df, x='dia', y='venda', marker='o', color="b", label="Vendas de gasolina")
+
+plt.title('Vendas Diárias de Gasolina', fontsize=16)
+plt.xlabel('Dia', fontsize=14)
+plt.ylabel('Venda (litros)', fontsize=14)
+
+plt.savefig('gasolina.png', dpi=300)
+
+plt.legend(title='Legenda')
+
+plt.show()
